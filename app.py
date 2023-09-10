c2724032c5af2c0dac1d949047107dd7e8c7f211
FND <fnd@localhost.localdomain>
2023-09-10 09:41:22 +0200

    added simplistic caching
    in the process, optimized rendering

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ TAG = "to-read"
 COUNT = 3
 TOKEN = os.environ["PINBOARD_API_TOKEN"]
 
+BOOKMARKS = None
+THRESHOLD = 5
+CACHE_HITS = THRESHOLD
+
 
 def app(environ, start_response):
     uri = environ.get("PATH_INFO", "")
@@ -22,13 +26,12 @@ def app(environ, start_response):
         start_response("404 Not Found", [("Content-Type", "text/plain")])
         return []
 
-    bookmarks = retrieve(TAG, COUNT)
     start_response("200 OK", [("Content-Type", "text/html")])
-    for html in render(bookmarks):
+    for html in render():
         yield html.encode("utf-8")
 
 
-def render(bookmarks):
+def render():
     yield """
 <!DOCTYPE html>
 <html lang="en">
@@ -38,7 +41,7 @@ def render(bookmarks):
 <h1>Bookmarks</h1>
 <ul>
     """.strip()
-    for bookmark in bookmarks:
+    for bookmark in retrieve(TAG, COUNT):
         html = '<a href="%s">%s</a>' % (escape(bookmark["href"], quote=True),
                 escape(bookmark["description"]))
 
@@ -51,11 +54,18 @@ def render(bookmarks):
 
 
 def retrieve(tag, count):
-    url = "%s?%s" % (BASE_URL, parse.urlencode({
-        "tag": tag,
-        "format": "json",
-        "auth_token": TOKEN
-    }))
-    res = request.urlopen(url)
-    bookmarks = json.load(res)
-    return choices(bookmarks, k=count)
+    global BOOKMARKS
+    global CACHE_HITS
+    if CACHE_HITS == THRESHOLD:
+        url = "%s?%s" % (BASE_URL, parse.urlencode({
+            "tag": tag,
+            "format": "json",
+            "auth_token": TOKEN
+        }))
+        res = request.urlopen(url)
+        BOOKMARKS = json.load(res)
+        CACHE_HITS = 0
+    else:
+        CACHE_HITS += 1
+
+    return choices(BOOKMARKS, k=count)
